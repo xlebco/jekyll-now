86bee02488c28c64476ee20d9be4c7573a79cc55
gorbachpaul2 <gorbachpaul@gmail.com>
2017-01-28 17:51:50 +0300

    Show/Hide Address details functionality
    #1 iteration
    JS file created
    Initial functionality is implemented

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+// select all address-details and add click listener to them
+const addressesLinks = document.querySelectorAll('.js-address');
+
+
+addressesLinks.forEach(block => block.addEventListener("click", function(event){
+    event.preventDefault()
+})); //prevents from moving page up upon click on a link
+addressesLinks.forEach(block => block.addEventListener('click', toggleHidden));
+
+
+// show and hide address-details on click
+
+function toggleHidden () {
+
+const addressDetails = this.nextElementSibling;
+addressDetails.classList.toggle('hidden');
+
+}
