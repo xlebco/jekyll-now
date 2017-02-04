cb60eb720c6a15096dd6004b06e4a0a0363adfab
gorbachpaul2 <gorbachpaul@gmail.com>
2017-02-04 14:15:10 +0300

    Index js refactor
    ES^ syntax is eliminated

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,23 @@
 // select all address-details and add click listener to them
-const addressesLinks = document.querySelectorAll('.js-address');
+var addressesLinks = document.querySelectorAll('.js-address');
 
 
-addressesLinks.forEach(block => block.addEventListener("click", function(event){
-    event.preventDefault()
-})); //prevents from moving page up upon click on a link
-addressesLinks.forEach(block => block.addEventListener('click', toggleHidden));
+addressesLinks.forEach(function (block) {
+  block.addEventListener("click", function(event){
+    //prevents from moving page up upon click on a link
+    event.preventDefault();
+}
+);} );
+addressesLinks.forEach(function (block) {
+  block.addEventListener('click', toggleHidden);});
 
 
 // show and hide address-details on click
 
 function toggleHidden () {
 
-const addressDetails = this.nextElementSibling;
-const address = this;
+var addressDetails = this.nextElementSibling;
+var address = this;
 address.classList.toggle('active');
 addressDetails.classList.toggle('hidden');
 
